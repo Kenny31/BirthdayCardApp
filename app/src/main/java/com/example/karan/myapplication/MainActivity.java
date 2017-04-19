42e1352f72b03fdba736663e7df1dd1023738476
Karan <karanbatra31@gmail.com>
2017-04-19 14:02:25 +0530

    Final Code

---
+++ app/src/main/java/com/example/karan/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.karan.myapplication;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.happy_birthday);
+
+        ImageView image = (ImageView) findViewById(R.id.pic);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            mediaPlayer.start();
+            }
+        });
+
+    }
+}
